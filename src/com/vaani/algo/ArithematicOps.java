a3c904bae68c36f5986b6160818207732f1f8e5a
Kinshuk <kinshuk.ram@gmail.com>
2011-12-27 08:53:45 +0530

    first commit

---
+++ src/com/vaani/algo/ArithematicOps.java
@@ -0,0 +1,32 @@
+package com.vaani.algo;
+
+public class ArithematicOps {
+	public static int getSumProperDivisors(int number)
+	{
+		int n=number;
+		int sum=1;
+		
+			int numOfDiv=0;
+			int loop=(int)Math.sqrt(number);
+
+			for (int i=2;i<=loop;i++)
+			{
+				if(n%i == 0)
+				{
+					numOfDiv += 2;
+					sum += i + n/i;
+				}
+			}
+
+			if(loop*loop==n)
+			{
+				numOfDiv--;
+				sum -= loop;
+			}
+
+			
+		return sum;
+	}
+
+	
+}
