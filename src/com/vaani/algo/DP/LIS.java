df90fe2a10d9a1d2c75309859d55dc1c3665c202
Kinshuk <kinshuk.ram@gmail.com>
2015-07-30 16:17:24 +0530

    Fixed compile error in LIS and Kth min elements in 2 sorted arrays

--- src/com/vaani/algo/DP/LIS.java
+++ src/com/vaani/algo/DP/LIS.java
@@ -17,7 +17,7 @@ public class LIS {
 	        //firstly we assign the intial values to each path/size, by setting size
 	        //to 1 and path equals the value (that means initially each path starting/ending
 	        //with its current position
-	        for(int i=0; i&lt;nums.length; i++) {
+	        for(int i=0; i<nums.length; i++) {
 	             sizes[i] = 1;
 	             paths[i] = nums[i] + " ";//we add a space for separation
 	        }
@@ -26,26 +26,26 @@ public class LIS {
 	        //we define a support variable called maxLength to keep track
 	        int maxLength = 1;//notice it is 1, but it really does not matter if sets to 0
 	        
-	        for(int i=1; i&lt;nums.length; i++) { 
+	        for(int i=1; i<nums.length; i++) { 
 	          //notice we start from 2nd postion, 1st position is no point
-	             for(int j=0; j&lt;i; j++) { //the inner loop is to check all previous items!
+	             for(int j=0; j<i; j++) { //the inner loop is to check all previous items!
 	                  //now it's the key step, 
 	              //when do we append our current index to the previous subsequence?
 	                  //it has to meet 2 requirements, 
-	              //current&gt;previous ending and size is increasing!
-	                  if(nums[i]&gt;nums[j] &amp;&amp; sizes[i] &lt; sizes[j]+1) { //notice plus one!!
+	              //current>previous ending and size is increasing!
+	                  if(nums[i]>nums[j]&& sizes[i] < sizes[j]+1) { //notice plus one!!
 	                        //if so, we need update sizes and path
 	                        sizes[i] = sizes[j]+1;
 	                        paths[i] = paths[j] + nums[i] + " ";//append current values to end!
 	                        //also update the maxLength if necessary
-	                        if(maxLength &lt; sizes[i])
+	                        if(maxLength < sizes[i])
 	                             maxLength = sizes[i];
 	                  }
 	             }
 	        }
 	        
 	        //finally go scanning the size array again and print out the path when size matches
-	        for(int i=1; i&lt;nums.length; i++) {
+	        for(int i=1; i<nums.length; i++) {
 	             if(sizes[i] == maxLength)
 	                  System.out.println("LIS: "+paths[i]);
 	        }
