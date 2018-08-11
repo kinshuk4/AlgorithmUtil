f9e9af975e646e58de0e32caf9baa602e7577801
Kinshuk <kinshuk.ram@gmail.com>
2018-08-11 09:10:06 +0200

    feat: Make the ListNode generic

--- src/com/vaani/algo/ds/core/list/ListNode.java
+++ src/com/vaani/algo/ds/core/list/ListNode.java
@@ -1,25 +1,25 @@
 package com.vaani.algo.ds.core.list;
 
-public class ListNode {
-    public int val;
-    public ListNode next;
+public class ListNode<T> {
+    public T val;
+    public ListNode<T> next;
 
 
-    public ListNode(int x) {
+    public ListNode(T x) {
         this.val = x;
         next = null;
     }
 
-    public ListNode(int x, ListNode next) {
+    public ListNode(T x, ListNode next) {
         this.val = x;
         this.next = next;
     }
 
-    public int getVal() {
+    public T getVal() {
         return val;
     }
 
-    public void setVal(int val) {
+    public void setVal(T val) {
         this.val = val;
     }
 
@@ -31,28 +31,9 @@ public class ListNode {
         this.next = next;
     }
 
-    public static boolean listEqual(ListNode head1, ListNode head2) {
-        if (head1 == null && head2 == null) {
-            return true;
-        } else if (head1 != null && head2 != null) {
-            return (head1.val == head2.val) && listEqual(head1.next, head2.next);
-        } else {
-            return false;
-        }
-    }
 
-    public static ListNode arrayToList(int[] arr) {
-        if (arr == null || arr.length == 0) {
-            return null;
-        }
-        ListNode head = new ListNode(arr[0]);
-        ListNode cur = head;
-        for (int i = 1; i < arr.length; ++i) {
-            cur.next = new ListNode(arr[i]);
-            cur = cur.next;
-        }
-        return head;
-    }
+
+
 
     public void display() {
         ListNode head = this;
