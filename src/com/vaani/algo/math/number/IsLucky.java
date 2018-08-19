b843daddca37abe95c5e99dca45683eada71b7b1
Kinshuk <kinshuk.ram@gmail.com>
2018-08-19 09:16:05 +0200

    fix: Import the right digitutils

--- src/com/vaani/algo/math/number/IsLucky.java
+++ src/com/vaani/algo/math/number/IsLucky.java
@@ -1,5 +1,7 @@
 package com.vaani.algo.math.number;
 
+import com.vaani.algo.ds.utils.DigitUtils;
+
 /**
  * Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
  * <p>
