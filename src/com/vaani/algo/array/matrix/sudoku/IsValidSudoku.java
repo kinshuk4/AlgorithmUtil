6c80e799ca9dec396e25aa8d6c9f5a7bcc9c347b
Kinshuk <kinshuk.ram@gmail.com>
2018-08-27 13:57:52 +0200

    feat: add integer based isValidSudoku

--- src/com/vaani/algo/array/matrix/sudoku/IsValidSudoku.java
+++ src/com/vaani/algo/array/matrix/sudoku/IsValidSudoku.java
@@ -5,12 +5,12 @@ import java.util.HashSet;
 
 public class IsValidSudoku {
 
-    public static boolean validate(char[] boardPart){
+    public static boolean validate(char[] boardPart) {
         HashSet<Character> hashSet = new HashSet<>();
-        for(int i = 0 ; i < boardPart.length; i++){
+        for (int i = 0; i < boardPart.length; i++) {
             char c = boardPart[i];
-            if (c != '.'){
-                if(hashSet.contains(c)){
+            if (c != '.') {
+                if (hashSet.contains(c)) {
                     return false;
                 }
                 hashSet.add(c);
@@ -44,7 +44,7 @@ public class IsValidSudoku {
         return true;
     }
 
-    public static boolean isCharacterRepeating(char c, HashSet<Character> hashSet){
+    public static boolean isCharacterRepeating(char c, HashSet<Character> hashSet) {
         if (c != '.') {
             if (hashSet.contains(c)) {
                 return true;
@@ -53,6 +53,7 @@ public class IsValidSudoku {
         }
         return false;
     }
+
     public boolean isValidSudoku(char[][] grid) {
         if (grid == null || grid.length != 9 || grid[0].length != 9)
             return false;
@@ -61,7 +62,7 @@ public class IsValidSudoku {
             HashSet<Character> hashSet = new HashSet<>();
             for (int j = 0; j < 9; j++) {
                 char c = grid[i][j];
-                if(isCharacterRepeating(c, hashSet)){
+                if (isCharacterRepeating(c, hashSet)) {
                     return false;
                 }
             }
@@ -73,7 +74,7 @@ public class IsValidSudoku {
             HashSet<Character> hashSet = new HashSet<>();
             for (int i = 0; i < 9; i++) {
                 char c = grid[i][j];
-                if(isCharacterRepeating(c, hashSet)){
+                if (isCharacterRepeating(c, hashSet)) {
                     return false;
                 }
             }
@@ -85,7 +86,57 @@ public class IsValidSudoku {
             for (int i = block / 3 * 3; i < block / 3 * 3 + 3; i++) {
                 for (int j = block % 3 * 3; j < block % 3 * 3 + 3; j++) {
                     char c = grid[i][j];
-                    if(isCharacterRepeating(c, hashSet)){
+                    if (isCharacterRepeating(c, hashSet)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isCharacterRepeatingInt(int c, HashSet<Integer> hashSet) {
+        if (hashSet.contains(c)) {
+            return true;
+        }
+        hashSet.add(c);
+        return false;
+    }
+
+    public boolean isValidSudokuInt(int[][] grid) {
+        if (grid == null || grid.length != 9 || grid[0].length != 9)
+            return false;
+        // check each column
+        for (int i = 0; i < 9; i++) {
+            HashSet<Integer> hashSet = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                int c = grid[i][j];
+                if (isCharacterRepeatingInt(c, hashSet)) {
+                    return false;
+                }
+            }
+        }
+
+        //check each row
+        for (int j = 0; j < 9; j++) {
+            boolean[] m = new boolean[9];
+            HashSet<Integer> hashSet = new HashSet<>();
+            for (int i = 0; i < 9; i++) {
+                int c = grid[i][j];
+                if (isCharacterRepeatingInt(c, hashSet)) {
+                    return false;
+                }
+            }
+        }
+
+        //check each 3*3 matrix
+        for (int block = 0; block < 9; block++) {
+            HashSet<Integer> hashSet = new HashSet<>();
+            for (int i = block / 3 * 3; i < block / 3 * 3 + 3; i++) {
+                for (int j = block % 3 * 3; j < block % 3 * 3 + 3; j++) {
+                    int c = grid[i][j];
+                    if (isCharacterRepeatingInt(c, hashSet)) {
                         return false;
                     }
                 }
