2a7cb2ab4950569694a84d3e5e6e6fb0a758f05d
Kinshuk <kinshuk.ram@gmail.com>
2018-08-30 08:56:24 +0200

    fix: Refactor the max histogram area problem

--- src/com/vaani/algo/array/container/LargestRectangleHistogram.java
+++ src/com/vaani/algo/array/container/LargestRectangleHistogram.java
@@ -9,34 +9,43 @@ import java.util.Stack;
  * Given height = [2,1,5,6,2,3],
  * return 10.
  * <p>
- * Created by Xiaomeng on 9/3/2014.
  */
 public class LargestRectangleHistogram {
     public static void main(String[] args) {
         LargestRectangleHistogram test = new LargestRectangleHistogram();
         int[] height = {2, 1, 5, 6, 2, 3};
-        System.out.println(test.largestRectangleArea(height));
+        System.out.println(largestRectangleArea(height));
+
+        System.out.println(largestRectangleArea(new int[] {1,2,3,4,5}));
     }
 
-    public int largestRectangleArea(int[] height) {
+    public static int largestRectangleArea(int[] height) {
         Stack<Integer> stack = new Stack<Integer>();
         int result = 0;
         int len = height.length;
-        for (int i = 0; i < height.length; i++) {
-            if (stack.isEmpty() || height[stack.peek()] < height[i]) {
+        for (int i = 0; i < height.length; ) {
+            if (stack.isEmpty() ||  height[i] >= height[stack.peek()]) {
                 stack.push(i);
+                i++;
             } else {
-                int index = stack.pop();
-                int sum = height[index] * (stack.isEmpty() ? i : i - stack.peek() - 1);
-                result = Math.max(sum, result);
-                i--;
+                result = calculateArea(height, stack, result, i);
             }
         }
         while (!stack.isEmpty()) {
-            int index = stack.pop();
-            int sum = height[index] * (stack.isEmpty() ? len : len - stack.peek() - 1);
-            result = Math.max(sum, result);
+            result = calculateArea(height, stack, result, len);
         }
         return result;
     }
+
+    private static int calculateArea(int[] height, Stack<Integer> stack, int result, int i) {
+        int index = stack.pop();
+        int h = height[index];
+        int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+        int sum = h * w;
+        result = Math.max(sum, result);
+        return result;
+    }
+
+
+
 }
