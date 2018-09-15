0bf0aee8f17539785189a46eec94642246b53939
Kinshuk <kinshuk.ram@gmail.com>
2018-09-15 21:41:49 +0200

    Add some cpp code 13

---
+++ cpp/src/main.cpp
@@ -0,0 +1,14 @@
+//akhil-and-gf.cpp
+//Akhil and GF
+//Ad Infinitum - Math Programming Contest August'14
+//Author: derekhh
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello world" << endl;
+	return 0;
+}
