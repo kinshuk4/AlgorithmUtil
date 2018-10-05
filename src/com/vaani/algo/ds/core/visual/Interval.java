f279372b67d6be370593a66c157d371ddefb5263
Kinshuk <kinshuk.ram@gmail.com>
2018-10-05 15:52:29 +0200

    Add the misc core code

--- src/com/vaani/algo/ds/core/visual/Interval.java
+++ src/com/vaani/algo/ds/core/visual/Interval.java
@@ -1,20 +1,23 @@
 package com.vaani.algo.ds.core.visual;
 
-/**
- * Created by Xiaomeng on 8/21/2014.
- */
-public class Interval {
+
+public class Interval implements Comparable<Interval> {
     public int start;
     public int end;
+    public int value; // some value between interval
 
     public Interval() {
-        start = 0;
-        end = 0;
+        this(0, 0);
     }
 
     public Interval(int start, int end) {
+        this(start, end, 0);
+    }
+
+    public Interval(int start, int end, int value) {
         this.start = start;
         this.end = end;
+        this.value = value;
     }
 
     @Override
@@ -23,5 +26,22 @@ public class Interval {
                 "start=" + start +
                 ", end=" + end +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Interval o) {
+        if (start < o.start) {
+            return -1;
+        } else if (start > o.start) {
+            return 1;
+        } else {
+            if (end < o.end) {
+                return -1;
+            } else if (end > end) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
     }
 }
