e72bd5f0c9bde3239e83553a8e7d76928a54e1c2
Kinshuk <kinshuk.ram@gmail.com>
2018-10-13 15:57:10 +0200

    Add code for Rat in a maze

--- src/com/vaani/algo/paradigm/backtracking/RatInAMaze2.java
+++ src/com/vaani/algo/paradigm/backtracking/RatInAMaze2.java
@@ -1,13 +1,14 @@
 package com.vaani.algo.paradigm.backtracking;
 
-/**https://www.geeksforgeeks.org/rat-in-a-maze-backtracking-2/
+/**
+ * https://www.geeksforgeeks.org/rat-in-a-maze-backtracking-2/
  */
-public class RatInAMaze {
+public class RatInAMaze2 {
 
     public int[][] solution;
 
     //initialize the solution matrix in constructor.
-    public RatInAMaze(int N) {
+    public RatInAMaze2(int N) {
         solution = new int[N][N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
@@ -17,7 +18,7 @@ public class RatInAMaze {
     }
 
     public void solve(int[][] maze, int N) {
-        if (findPath(maze, 0, 0, N)) {
+        if (findPath(maze, 0, 0, N, "down")) {
             print(solution, N);
         } else {
             System.out.println("NO PATH FOUND");
@@ -25,7 +26,7 @@ public class RatInAMaze {
 
     }
 
-    public boolean findPath(int[][] maze, int x, int y, int N) {
+    public boolean findPath(int[][] maze, int x, int y, int N, String direction) {
         // check if maze[x][y] is feasible to move
         if (x == N - 1 && y == N - 1) {//we have reached
             solution[x][y] = 1;
@@ -35,11 +36,17 @@ public class RatInAMaze {
             // move to maze[x][y]
             solution[x][y] = 1;
             // now rat has four options, either go right OR go down or left or go up
-            if (findPath(maze, x + 1, y, N)) { //go down
+            if (direction != "up" && findPath(maze, x + 1, y, N, "down")) { //go down
                 return true;
             }
             //else go down
-            if (findPath(maze, x, y + 1, N)) { //go right
+            if (direction != "left" && findPath(maze, x, y + 1, N, "right")) { //go right
+                return true;
+            }
+            if (direction != "down" && findPath(maze, x - 1, y, N, "up")) { //go up
+                return true;
+            }
+            if (direction != "right" && findPath(maze, x, y - 1, N, "left")) { //go left
                 return true;
             }
             //if none of the options work out BACKTRACK undo the move
@@ -68,14 +75,11 @@ public class RatInAMaze {
     }
 
     public static void main(String[] args) {
-        int N = 4;
-        int[][] maze = {{1, 0, 0, 0},
-                {1, 1, 0, 0},
-                {0, 1, 0, 0},
-                {0, 1, 1, 1}};
-        RatInAMaze r = new RatInAMaze(N);
+        int N = 5;
+        int[][] maze = {{1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 1, 1},
+                {0, 0, 0, 1, 0}, {0, 0, 0, 1, 1}};
+        RatInAMaze2 r = new RatInAMaze2(N);
         r.solve(maze, N);
     }
-
 
 }
