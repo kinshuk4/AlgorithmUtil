bf52b948394892f68fe5588deb518c36e8f71f16
Kinshuk <kinshuk.ram@gmail.com>
2018-10-14 15:57:29 +0200

    Fix some code for LCS

--- src/com/vaani/algo/paradigm/dp/LongestCommonSubsequence.java
+++ src/com/vaani/algo/paradigm/dp/LongestCommonSubsequence.java
@@ -8,23 +8,22 @@ package com.vaani.algo.paradigm.dp;
  * LCS for input Sequences “ABCDGH” and “AEDFHR” is “ADH” of length 3.
  * LCS for input Sequences “AGGTAB” and “GXTXAYB” is “GTAB” of length 4.
  * <p>
- * Created by Xiaomeng on 11/5/2014.
  */
 public class LongestCommonSubsequence {
     /**
      * Time(2 ^ n)
      */
-    public static int findLCS(String S, String T) {
-        return findLCS(S, T, S.length() - 1, T.length() - 1);
+    public static int findLcsRecursion(String S, String T) {
+        return findLcsRecursion(S, T, S.length() - 1, T.length() - 1);
     }
 
-    public static int findLCS(String S, String T, int m, int n) {
+    public static int findLcsRecursion(String S, String T, int m, int n) {
         if (m < 0 || n < 0) {
             return 0;
         } else if (S.charAt(m) == T.charAt(n)) {
-            return findLCS(S, T, m - 1, n - 1) + 1;
+            return findLcsRecursion(S, T, m - 1, n - 1) + 1;
         } else {
-            return Math.max(findLCS(S, T, m, n - 1), findLCS(S, T, m - 1, n));
+            return Math.max(findLcsRecursion(S, T, m, n - 1), findLcsRecursion(S, T, m - 1, n));
         }
     }
 
@@ -32,26 +31,38 @@ public class LongestCommonSubsequence {
      * Time(m * n), Space (m * n)
      * Reference: http://www.geeksforgeeks.org/dynamic-programming-set-4-longest-common-subsequence/
      */
-    public static int findLCSDP(String S, String T) {
+    public static int findLcsDP(String S, String T) {
+        int m = S.length();
+        int n = T.length();
+        int[][] memo = getLcsMemoArray(S, T);
+        return memo[m][n];
+    }
+
+    public static int[][] getLcsMemoArray(String S, String T) {
         int m = S.length();
         int n = T.length();
         int[][] memo = new int[m + 1][n + 1];
 
         for (int i = 0; i < m + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                if (i == 0 || j == 0) {
-                    memo[i][j] = 0;
+            memo[i][0] = 0;
+        }
+
+        for (int j = 0; j < n + 1; j++) {
+            memo[0][j] = 0;
+        }
+
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (S.charAt(i - 1) == T.charAt(j - 1)) {
+                    memo[i][j] = memo[i - 1][j - 1] + 1;
                 } else {
-                    if (S.charAt(i - 1) == T.charAt(j - 1)) {
-                        memo[i][j] = memo[i - 1][j - 1] + 1;
-                    } else {
-                        memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
-                    }
+                    memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
                 }
             }
         }
-        return memo[m][n];
+        return memo;
     }
+
 
     /**
      * Reference: http://www.geeksforgeeks.org/printing-longest-common-subsequence/
@@ -59,21 +70,7 @@ public class LongestCommonSubsequence {
     public static String printLCS(String S, String T) {
         int m = S.length();
         int n = T.length();
-        int[][] memo = new int[m + 1][n + 1];
-
-        for (int i = 0; i < m + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                if (i == 0 || j == 0) {
-                    memo[i][j] = 0;
-                } else {
-                    if (S.charAt(i - 1) == T.charAt(j - 1)) {
-                        memo[i][j] = memo[i - 1][j - 1] + 1;
-                    } else {
-                        memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
-                    }
-                }
-            }
-        }
+        int[][] memo = getLcsMemoArray(S, T);
 
         StringBuilder result = new StringBuilder();
         int i = m, j = n;
@@ -95,7 +92,7 @@ public class LongestCommonSubsequence {
         String S = "AGGTAB";
         String T = "GXTXAYB";
 
-        System.out.println(findLCSDP(S, T));
+        System.out.println(findLcsDP(S, T));
         System.out.println(printLCS(S, T));
     }
 }
